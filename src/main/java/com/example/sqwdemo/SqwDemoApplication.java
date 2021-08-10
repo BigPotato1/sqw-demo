cff4bd33d3a145c23368fc1a115eb3af84e91f66
沈青文 <shenqingwen69@qq.com>
2021-08-10 16:14:45 +0800

    项目初始化

---
+++ src/main/java/com/example/sqwdemo/SqwDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqwdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqwDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqwDemoApplication.class, args);
+    }
+
+}
